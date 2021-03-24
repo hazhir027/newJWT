ae14179dc29b2a96d65e840722f079a64943eb19
Hazhir <kaa.hazhir@gmail.com>
2021-03-24 13:10:33 +0430

    newJWT

---
+++ app/User.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App;
+
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Tymon\JWTAuth\Contracts\JWTSubject;
+
+class User extends Authenticatable implements JWTSubject
+{
+    use Notifiable;
+
+    protected $fillable = [
+        'name', 'email', 'password',
+    ];
+
+
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+
+
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+
+
+    /*--- JWTSubject methods are below*/
+
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
+
+
+}
